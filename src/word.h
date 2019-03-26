53fa79e725f5eb72dcb77fd4f3358428f52a806c
suchang <1103232282@qq.com>
2019-03-26 09:36:34 +0800

    标准化

--- src/word.h
+++ src/word.h
@@ -5,13 +5,23 @@
 #include <vector>
 #include "lex.h"
 
+enum TreeNodeType
+{
+  ROOT,
+  NORMAL,
+  EMPTY,
+};
+
 class ExpNode
 {
+public:
+  int id;
   std::string value;
-  Token &token;
-  ExpNode &left_node;
-  ExpNode &right_node;
-  ExpNode &parent_node;
+  Token *token;
+  ExpNode *left_node;
+  ExpNode *right_node;
+  ExpNode *parent_node;
+  TreeNodeType type;
 };
 
 class WordStack
