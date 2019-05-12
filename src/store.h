729afbad6f6e9fbbe218aca08f8c17f35273aacb
suchang <1103232282@qq.com>
2019-05-12 20:06:25 +0800

    test

--- src/store.h
+++ src/store.h
@@ -1,4 +1,3 @@
-#pragma once
 #include <string>
 #include <vector>
 #include "text.h"
@@ -7,31 +6,36 @@
 class AbcStore
 {
 public:
-  std::vector<TextBlock> data;
-  virtual int Save(std::string path) = 0;
-  virtual int Read(std::string path) =0;
+    std::vector<TextBlock> data;
+    virtual int Save(std::string path) = 0;
 };
 
 class NatureStore : public AbcStore
 {
 public:
-  int Save(std::string path);
+    int Save(std::string path);
 };
 
 class ConceptStore : public AbcStore
 {
 public:
-  int Save(std::string path);
+    int Save(std::string path);
 };
 
 class ExercisesStore : public AbcStore
 {
 public:
-  int Save(std::string path);
+    int Save(std::string path);
+};
+
+class ExampleStore : public AbcStore
+{
+public:
+    int Save(std::string path);
 };
 
 class TextStore : public AbcStore
 {
 public:
-  int Save(std::string path);
+    int Save(std::string path);
 };
---
+++ test/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 苏畅 on 2019/5/12.
+//
+
+#include<iostream>
+#include "interface.h"
+#include "text.h"
+int main()
+{
+    auto r = ParseFromLatex("/Users/suchang/Code/homework/math_search/LaTexDoc/book56-60.tex");
+    return  0;
+}
