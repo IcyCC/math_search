225c6191f4c2c81b67cbe4b474e10354dd953319
suchang <1103232282@qq.com>
2019-05-29 11:25:12 +0800

    测通

--- src/pyinterface.cpp
+++ src/pyinterface.cpp
@@ -10,9 +10,24 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(pyinterface, m) {
-m.doc() = "pybind11 example plugin"; // optional module docstring
 
-py::class_<query::QueryResult>(m, 'QueryResult');
+py::class_<query::QueryResult> query_result(m, "QueryResult");
+
+query_result.def(py::init<>());
+query_result.def_readwrite("chapter", &query::QueryResult::chapter);
+query_result.def_readwrite("id", &query::QueryResult::id);
+query_result.def_readwrite("title", &query::QueryResult::title);
+query_result.def_readwrite("summary", &query::QueryResult::summary);
+query_result.def_readwrite("raw", &query::QueryResult::raw);
+
+
+py::enum_<query::QueryResult::BlockType >(query_result, "BlockType")
+.value("NATURE", query::QueryResult::BlockType::NATURE)
+.value("CONCEPT", query::QueryResult::BlockType::CONCEPT)
+.value("EXERCISES", query::QueryResult::BlockType::EXERCISES)
+.value("TEXT", query::QueryResult::BlockType::TEXT)
+.value("EXAMPLE", query::QueryResult::BlockType::EXAMPLE)
+.export_values();
 
 m.def("SegmentWords", &SegmentWords, "");
 m.def("QueryCommPy", &QueryComm, "");
